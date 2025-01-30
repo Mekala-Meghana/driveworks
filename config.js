9571bd8460c3f04008752fd421916a738d71d914
Mekala-Meghana <135518971+Mekala-Meghana@users.noreply.github.com>
2025-01-30 10:15:34 +0530

    Added

---
+++ config.js
@@ -0,0 +1,50 @@
+// Update these values to match those of your Server URL & DriveWorks Group Alias
+
+const config = {
+    serverUrl: "http://www.cmtruckslive.com",
+    groupAlias: "CmTrucksLive",
+    // (Optional) Configure ping & update intervals - in seconds
+    // A Specification will timeout after a configured period of inactivity (see DriveWorksConfigUser.xml).
+    // This function prevents a Specification timing out as long as the page is in view.
+    // Disable the ping by setting to 0
+    specificationPingInterval: 0,
+    // (Optional) Enter custom redirect URLs for login/logout and Project/DriveApp close/cancel
+    login: {
+        redirectUrl: "projects.html",
+    },
+    logout: {
+        redirectUrl: "index.html",
+    },
+    project: {
+        redirectOnClose: "details.html",
+        redirectOnCancel: "projects.html",
+    },
+    driveApp: {
+        redirectOnClose: "details.html",
+        redirectOnCancel: "drive-apps.html",
+    },
+    // (Optional) Configure 'Run' view
+    run: {
+        showWarningOnExit: false, // Toggle warning dialog when exiting "Run" view with potentially unsaved changes (where supported)
+        loadCustomProjectAssets: {
+            scripts: false,
+            styles: false,
+        }
+    },
+    // (Optional) Configure 'Details' view
+    details: {
+        updateInterval: 5, // Interval to refresh content - in seconds
+        showStartNewSpecificationAction: true,
+    },
+    // (Optional) Configure the query function
+    // Enter a default Group Alias and/or Project name to be used (when none are passed in the query string)
+    // Choose how sessions are handled
+    query: {
+        defaultGroupAlias: "",
+        defaultProjectName: "",
+        autoLogin: false,
+        requireNewSession: false,
+        requireExactAlias: false,
+    },
+    loginReturnUrls: true, // Toggle appending return urls to restore the previous location when redirected to the login form 
+};
