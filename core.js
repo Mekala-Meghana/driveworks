adb308595e50b90d55bbfffb388b0e4041305d4e
Mekala-Meghana <135518971+Mekala-Meghana@users.noreply.github.com>
2025-01-30 10:33:55 +0530

    Added

---
+++ core.js
@@ -0,0 +1,269 @@
+/**
+ * SHARED CORE PAGE FUNCTIONS
+ */
+
+const SERVER_URL = config.serverUrl;
+const GROUP_ALIAS = localStorage.getItem("sessionAlias");
+const CURRENT_SESSION = localStorage.getItem("sessionId");
+
+let client;
+
+/**
+ * Run on page load.
+ */
+(() => {
+    // Check if Session Id exists
+    checkStoredSessionId();
+
+    showUsername();
+    attachLogoutActions();
+    detectTouchDevice();
+    handleMobileNavigationToggle();
+})();
+
+/**
+ * DriveWorks Live client library loaded.
+ */
+function dwClientLoaded() {
+    try {
+        // Create client
+        client = new window.DriveWorksLiveClient(SERVER_URL);
+
+        // Set session id from stored value - set by and passed from login page
+        client._sessionId = getLocalSession();
+    } catch (error) {
+        dwClientLoadError();
+        return;
+    }
+
+    // Quick Logout (?bye)
+    // https://docs.driveworkspro.com/Topic/WebThemeLogout
+    const coreQuery = new URLSearchParams(window.location.search);
+    if (coreQuery.has("bye")) {
+        handleLogout();
+        return;
+    }
+
+    // Start individual page functions
+    startPageFunctions();
+}
+
+/**
+ * DriveWorks Live client library load error.
+ */
+function dwClientLoadError() {
+    redirectToLogin("Cannot access client.", "error");
+}
+
+/**
+ * Check Session Id exists locally.
+ */
+async function checkStoredSessionId() {
+    // If no session is stored (e.g. not logged in), redirect to login
+    if (CURRENT_SESSION === null || CURRENT_SESSION === "undefined") {
+        handleUnauthorizedUser();
+    }
+}
+
+/**
+ * Add generic client error handling for unauthorized users.
+ */
+async function setCustomClientErrorHandler() {
+    client.responseErrorDelegate = (res) => {
+        if (res.status == 401) {
+            handleUnauthorizedUser();
+        }
+    }
+}
+
+/**
+ * Set login screen notice.
+ * 
+ * @param {string} text - The text displayed to the user on the login screen.
+ * @param {string} [state] - The type of message state (error/success/info).
+ */
+function setLoginNotice(text, state = "info") {
+    const notice = JSON.stringify({text, state});
+    localStorage.setItem("loginNotice", notice);
+}
+
+/**
+ * Redirect to login screen.
+ * 
+ * @param {string} notice - The text displayed to the user on the login screen.
+ * @param {string} state - The type of message state (error/success/info).
+ * @param {boolean} [noReturnUrl] - Optionally disable return url
+ */
+function redirectToLogin(notice, state, noReturnUrl) {
+
+    // Clear Session from storage
+    localStorage.clear();
+
+    // Store login screen message
+    if (notice && state) {
+        setLoginNotice(notice, state);
+    }
+
+    // Redirect to login
+    if (noReturnUrl || !config.loginReturnUrls) {
+        window.location.replace("index.html");
+        return;
+    }
+
+    // Redirect to login, with return url to restore position
+    const currentLocation = window.location.pathname + window.location.search;
+    window.location.replace(`index.html?returnUrl=${encodeURIComponent(currentLocation.substring(1))}`);
+}
+
+/**
+ * Attach logout actions.
+ */
+function attachLogoutActions() {
+    const logoutButtons = document.getElementsByClassName("logout-button");
+    if (!logoutButtons) {
+        return;
+    }
+
+    for (const logoutButton of logoutButtons) {
+        logoutButton.addEventListener("click", handleLogout);
+    }
+}
+
+/**
+ * Redirect on logout.
+ */
+function logoutRedirect() {
+
+    // Clear Session from storage
+    localStorage.clear();
+
+    // Redirect
+    window.location.replace(config.logout.redirectUrl);
+}
+
+/**
+ * Logout action.
+ */
+async function handleLogout() {
+    try {
+        await client.logoutAllGroups();
+        logoutRedirect();
+    } catch (error) {
+        handleGenericError(error);
+    }
+}
+
+/**
+ * Handle unauthorized users.
+ * 
+ * @param {Object} error - Object representing originating error.
+ */
+function handleUnauthorizedUser(error) {
+    let message = "Please login to view that.";
+    if (error) {
+        message = error;
+    }
+
+    redirectToLogin(message, "error");
+}
+
+/**
+ * Mobile navigation toggle.
+ */
+function handleMobileNavigationToggle() {
+    const navList = document.getElementById("nav-list");
+    if (!navList) {
+        return;
+    }
+
+    document.getElementById("nav-toggle").onclick = function () {
+        document.body.classList.toggle("sidebar-open");
+    };
+
+    document.getElementById("nav-close").onclick = function () {
+        document.body.classList.remove("sidebar-open");
+    };
+}
+
+/**
+ * Split string on uppercase.
+ * "MyStringValue" => "My String Value".
+ * 
+ * @param {string} string - The string to split.
+ */
+function splitOnUpperCase(string) {
+    return string.split(/(?=[A-Z])/).join(" ");
+}
+
+/**
+ * Format string to remove (clean) special characters, lowercase & hyphenate whitespace for class & file names.
+ * e.g. "Total (Plus VAT) " => "total-plus-vat".
+ * 
+ * @param {string} string - The string to normalize.
+ */
+function normalizeString(string) {
+    string = string.toLowerCase();
+    string = string.replaceAll(/[^a-zA-Z0-9 _-]/g, ""); // Strip all characters excluding: alphanumeric, whitespace, underscore, hyphen
+    string = string.trim(); // Remove outer whitespace
+    string = string.replaceAll(/ +/g, "-"); // Convert whitespace to hyphens
+    return string;
+}
+
+/**
+ * Check if 2 objects are equal.
+ * 
+ * @param {Object} a - The 1st object to compare.
+ * @param {Object} b - The 2nd object to compare.
+ */
+function objectsEqual(a, b) {
+    return JSON.stringify(a) === JSON.stringify(b);
+}
+
+/**
+ * Check if object is empty.
+ * 
+ * @param {Object} obj - The object to check for content.
+ */
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+/**
+ * Visual output of username.
+ */
+function showUsername() {
+    const usernameOutput = document.getElementById("active-username");
+    const username = localStorage.getItem("sessionUsername");
+    if (!username || !usernameOutput) {
+        return;
+    }
+
+    usernameOutput.classList.add("is-shown");
+    document.querySelector("#active-username .username").innerHTML = username;
+}
+
+/**
+* Detect touch devices - alter UI accordingly.
+*/
+function detectTouchDevice() {
+    try {
+        document.createEvent("TouchEvent");
+        document.body.classList.add("touch");
+    } catch (error) {
+        document.body.classList.add("no-touch");
+    }
+}
+
+/**
+ * Get local Session Id from storage - between pages.
+ */
+getLocalSession = () => localStorage.getItem("sessionId");
+
+/**
+ * Handle generic error.
+ * 
+ * @param {Object} error - Error object to handle.
+ */
+function handleGenericError(error) {
+    console.log(error);
+}
---
+++ login.js
@@ -0,0 +1,320 @@
+/**
+ * LOGIN
+ */
+
+const SERVER_URL = config.serverUrl;
+const LOGIN_REDIRECT_URL = config.login.redirectUrl;
+const GROUP_ALIAS = config.groupAlias;
+const URL_QUERY = new URLSearchParams(window.location.search);
+
+// Elements
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-button");
+const loginSSOButton = document.getElementById("login-sso-button");
+const loginNotice = document.getElementById("login-notice");
+
+// Error Messages
+const genericErrorMessage = "There has been an issue.";
+const clientErrorMessage = "Cannot access client.";
+const privateErrorMessage = "Please use a non-private window.";
+
+// DriveWorks Live Client
+let client;
+
+/**
+ * On page load.
+ */
+(async function() {
+    loginForm.addEventListener("submit", handleLoginForm);
+
+    if (loginSSOButton) {
+        loginSSOButton.addEventListener("click", handleLoginSSO);
+    }
+
+    showLoginNotice();
+})();
+
+/**
+ * Create client.
+ */
+async function dwClientLoaded() {
+    try {
+        client = new window.DriveWorksLiveClient(SERVER_URL);
+    } catch (error) {
+        loginError(clientErrorMessage, error);
+    }
+
+    // Quick Logout (?bye)
+    // https://docs.driveworkspro.com/Topic/WebThemeLogout
+    if (URL_QUERY.has("bye")) {
+        await forceLogout();
+    }
+
+    startPageFunctions();
+}
+
+/**
+ * Start page functions.
+ */
+function startPageFunctions() {
+    handlePasswordToggle();
+
+    // Check localStorage support (show warning if not e.g. <= iOS 10 Private Window)
+    if (!localStorageSupported()) {
+        document.getElementById("login-button").disabled = true;
+        loginError(privateErrorMessage);
+        return;
+    }
+
+    try {
+        // Check if logged in, and redirect
+        checkExistingLogin();
+    } catch (error) {
+        handleGenericError(error);
+    }
+}
+
+/**
+ * Handle login with server using credentials.
+ *
+ * @param {Object} event - Form submit event.
+ */
+async function handleLoginForm(event) {
+
+    // Prevent default form handling
+    event.preventDefault();
+
+    // Show error if cannot connect to client
+    if (!client) {
+        loginError(clientErrorMessage);
+        return;
+    }
+
+    // Get credentials
+    const inputUsername = document.getElementById("login-username").value;
+    const inputPassword = document.getElementById("login-password").value;
+    const userCredentials = {
+        username: inputUsername,
+        password: inputPassword
+    };
+
+    try {
+        // Show loading state, reset notice
+        loginButton.classList.add("is-loading");
+        hideLoginNotice();
+
+        // Start Session
+        const result = await client.loginGroup(GROUP_ALIAS, userCredentials);
+
+        // Show error is login failed
+        if (!result) {
+            loginError(genericErrorMessage);
+            return;
+        }
+
+        loginSuccess(result, inputUsername);
+    } catch (error) {
+        loginError(genericErrorMessage, error);
+    }
+}
+
+/**
+ * Handle DriveWorks Group login via Single Sign-On (SSO).
+ */
+async function handleLoginSSO() {
+
+    // Show error if cannot connect to client
+    if (!client) {
+        loginError(clientErrorMessage);
+        return;
+    }
+
+    try {
+        hideLoginNotice();
+
+        // Start session
+        const result = await client.loginSSO(GROUP_ALIAS);
+
+        // Show error is login failed
+        if (!result) {
+            loginError(genericErrorMessage);
+            return;
+        }
+
+        loginSuccess(result);
+    } catch (error) {
+        loginError(genericErrorMessage, error);
+    }
+}
+
+/**
+ * Handle successful login. Store Session data to localStorage & redirect.
+ */
+function loginSuccess(result, username) {
+
+    // Store session details to localStorage
+    localStorage.setItem("sessionId", result.sessionId);
+    localStorage.setItem("sessionAlias", GROUP_ALIAS);
+
+    if (username) {
+        localStorage.setItem("sessionUsername", username);
+    }
+
+    // Return to previous location (if redirected to login)
+    const returnUrl = URL_QUERY.get("returnUrl");
+
+    if (returnUrl && config.loginReturnUrls) {
+        window.location.href = `${window.location.origin}/${decodeURIComponent(returnUrl)}`;
+        return;
+    }
+
+    // Redirect to default location
+    window.location.href = LOGIN_REDIRECT_URL;
+}
+
+/**
+ * Handle login errors.
+ *
+ * @param {string} noticeText - The message to display when directed to the login screen.
+ * @param {Object} [error] - The error object.
+ */
+function loginError(noticeText, error = null) {
+    if (error) {
+        handleGenericError(error);
+    }
+
+    // Remove loading state
+    loginButton.classList.remove("is-loading");
+
+    // Show client error
+    setLoginNotice(noticeText, "error");
+    showLoginNotice();
+}
+
+/**
+ * Set login screen notice.
+ *
+ * @param {string} text - The message to display when directed to the login screen.
+ * @param {string} [state] - The type of message state (error/success/info).
+ */
+function setLoginNotice(text, state = "info") {
+    const notice = JSON.stringify({ text: text, state: state });
+    localStorage.setItem("loginNotice", notice);
+}
+
+/**
+ * Show notice on login form.
+ */
+function showLoginNotice() {
+    const notice = JSON.parse(localStorage.getItem("loginNotice"));
+
+    if (!notice) {
+        return;
+    }
+
+    let state = notice.state;
+
+    if (!state) {
+        state = "neutral";
+    }
+
+    // Display feedback
+    loginNotice.innerText = notice.text;
+    loginNotice.classList.remove("error", "success", "neutral");
+    loginNotice.classList.add(state, "is-shown");
+
+    // Clear message
+    localStorage.removeItem("loginNotice");
+}
+
+/**
+ * Hide notice on login form.
+ */
+function hideLoginNotice() {
+    loginNotice.classList.remove("is-shown");
+}
+
+/**
+ * Handle password visibility toggle.
+ */
+function handlePasswordToggle() {
+    const passwordToggle = document.getElementById("password-toggle");
+
+    passwordToggle.onclick = function () {
+        const passwordInput = document.getElementById("login-password");
+        const currentType = passwordInput.type;
+
+        if (currentType === "password") {
+            passwordInput.type = "text";
+            passwordToggle.innerHTML = '<svg class="icon"><use xlink:href="dist/icons.svg#eye-closed"/></svg> Hide';
+            return;
+        }
+
+        passwordInput.type = "password";
+        passwordToggle.innerHTML = '<svg class="icon"><use xlink:href="dist/icons.svg#eye-open"/></svg> Show';
+    };
+}
+
+/**
+ * Check existing login. Automatically login if found.
+ */
+async function checkExistingLogin() {
+    const storedGroupAlias = localStorage.getItem("sessionAlias");
+
+    if (!storedGroupAlias) {
+        return;
+    }
+
+    try {
+        // Test connection
+        await client.getProjects(storedGroupAlias, "$top=1");
+
+        // Redirect to initial location
+        window.location.replace(LOGIN_REDIRECT_URL);
+    } catch (error) {
+        handleGenericError(error);
+    }
+}
+
+/**
+ * Force logout and session data clearing.
+ */
+async function forceLogout() {
+
+    // Logout from all Groups.
+    try {
+        await client.logoutAllGroups();
+    } catch(error) {
+        handleGenericError(error);
+    }
+
+    // Clear session information from storage.
+    localStorage.clear();
+
+    // Show login screen message.
+    setLoginNotice("You have been logged out.", "success");
+    showLoginNotice();
+}
+
+/**
+ * Check for localStorage support - used to store session information.
+ * Example: Incognito (Private) windows in iOS 10 and below do not allow localStorage, errors when accessed.
+ */
+function localStorageSupported() {
+    try {
+        localStorage.setItem("storageSupportTest", "Test");
+        localStorage.removeItem("storageSupportTest");
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+/**
+ * Handle generic errors e.g. tryCatch.
+ *
+ * @param {Object} error - The error object.
+ */
+function handleGenericError(error) {
+    console.log(error);
+}
